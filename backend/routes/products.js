d9e6f319d1721bd716840cd9806ff0aef71d5962
Sandali Yaddehige <sandaliyaddehige6@gmail.com>
2026-04-14 21:10:11 +0530

    backend/models/product.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,33 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/Product'); 
+const Product = require('../models/product');
 
 
 router.get('/', async (req, res) => {
     try {
-        const products = await Product.find();
+        const products = await Product.find().sort({ createdAt: -1 });
         res.json(products);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
-// 2. අලුත් Product එකක් ඇතුළත් කිරීම (POST)
-router.post('/', async (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        sku: req.body.sku,
-        category: req.body.category,
-        price: req.body.price,
-        cost: req.body.cost,
-        stock: req.body.stock
-    });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).json({ message: "Product not found" });
+        res.json(product);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    const product = new Product(req.body);
     try {
         const newProduct = await product.save();
         res.status(201).json(newProduct);
     } catch (err) {
         res.status(400).json({ message: err.message });
+    }
+});
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.json({ message: "Product deleted" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 });
 
